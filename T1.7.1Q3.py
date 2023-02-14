df70996e2b94fd7cc3472a357e934b855f514b54
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-14 12:58:41 +0200

    kehitysympäristö tehtävä

---
+++ T1.7.1Q3.py
@@ -0,0 +1,11 @@
+print(max (10, 16, 30, 21, 25, 28))
+print(min(10,16, 30, 21, 25, 28))
+print('bang'.upper ())
+
+message = 'Python on minusta hieno'
+print(message.replace ('hieno', ':D'))
+print('Python on minusta :D')
+
+countdown = [1, 2, 3]
+countdown.reverse ()
+print(countdown)
