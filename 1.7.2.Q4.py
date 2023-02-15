f7e87b6ca1b77659768c7fe556c765d5634d1fb5
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-15 16:55:31 +0200

    tehtäviä

---
+++ 1.7.2.Q4.py
@@ -0,0 +1,9 @@
+def convert_day_to_seconds (days) :
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+    return seconds
+
+total_seconds = convert_day_to_seconds (7)
+milliseconds = total_seconds * 1000
+print(milliseconds)
