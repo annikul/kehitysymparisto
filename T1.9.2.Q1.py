f9d5f2f6d726d27ee6c67b627107075e725212de
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-18 00:01:54 +0200

    tehty lisää tehtäviä

---
+++ T1.9.2.Q1.py
@@ -0,0 +1,9 @@
+import math
+
+x = math.sqrt (64)
+y = math.floor (1.5) # math.floor = pyöristää alaspäin
+z = math.ceil (1.5)
+
+print(x)
+print(y)
+print(z)
---
+++ T1.9.2.Q2.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+nouns = ['omena', 'dinosaurus', 'pallo','paahdin','vuohi','lohikäärme','vasara','sorsa','panda','puhelin','banaani','opettaja']
+
+noun = random.choice(nouns)
+print(noun)
