86519fbf9e8c3d09ddbc1dcf4c1277d2aca830ac
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-17 11:45:56 +0200

    päivitetty

---
+++ T1.2.Q1.py
@@ -0,0 +1,17 @@
+print('Olen 10-vuotias')
+print(123 + 456 * 7 / 8)
+print(''.join(reversed('Kohta ohjelmoidaan')))
+
+# Tehtävä tehdäään oikeasti terminaaliin
+# Ensiksi python päälle terminaalissa: python3
+# print('Olen 10-vuotias')
+# 123 + 456 * 7 / 8
+# ''.join(reversed('Kohta ohjelmoidaan'))
+# Terminaaliin tulostuu: 
+# >>> print ('Olen 10-vuotias')
+# Olen 10-vuotias
+# >>> 123 + 456 * 7 / 8
+# 522.0
+# >>> ''.join(reversed ('Kohta ohjelmoidaan'))
+# 'naadiomlejho athoK'
+# Pois terminaali päältä CTRL ja Z
--- T1.4.2.Q3.py
+++
@@ -1,6 +0,0 @@
-# Kysytään henkilön nimi
-person = input('Mikä on nimesi?')
-if (person == 'Jussi'):
-    print ('Terve', person)
-else:
-    print ('Nimen pituus =', len(person))
