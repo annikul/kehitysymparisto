ec3334b1b9f03896894a96bb1c9059ebd0bbf7fd
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-22 19:08:40 +0200

    tehtävä

---
+++ T1.9.3.Q2.py
@@ -0,0 +1,40 @@
+# Tulostaa sinulle random salasanoja 
+import random
+import string
+nouns = ['omena','dinosaurus','pallo','paahdin','vuohi',
+'lohikäärme','vasara','sorsa','panda','puhelin','banaani','opettaja']
+
+adjectives = ['uninen', 'hidas', 'haiseva', 'märkä', 'lihava', 
+'punainen','oranssi', 'keltainen', 'vihreä','sininen', 'purppura', 
+'pörröinen','valkoinen', 'leuhka', 'uljas']
+
+ 
+
+while True:
+
+    # valitaan satunnaisesti adjectives listalta
+
+        adjective = random.choice(adjectives)
+
+    # valitaan satunnaisesti nouns listalta
+
+        noun = random.choice (nouns)
+
+    # satunnainen numero väliltä 0...99
+
+        number = random.randrange(0, 100)
+
+        special_char= random.choice(string.punctuation)
+
+    # rakennetaan salasana
+
+        password = adjective + noun + str(number) + special_char
+
+        print(' Uusi salasanasi on: %s' % password)
+
+        response = input("Haluatko toisen salasanan? Vastaa k tai e ")
+
+        if response == 'e':
+
+            break
+
