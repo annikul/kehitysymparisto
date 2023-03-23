77074e389852bd285c59975b3e943bb6f8a03885
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-23 18:52:24 +0200

    tahtitaivas valmis

--- T2.7.tahtikirkas_yo_turtle.py
+++ T2.7.tahtikirkas_yo_turtle.py
@@ -11,30 +11,32 @@ from random import randint, random
 x = 0
 y = 0
 
-def draw_star (points, size, col, x, y):
+def draw_star(points, size, col, x, y):
     piirto_menossa = True
+    t.speed(6)
+    t.penup()
     t.goto(x, y)
-    t.pendown
+    t.pendown()
     angle = 180 -(180 / points)
-    t.color (col)
-    t.begin_fill ()
+    t.color(col)
+    t.begin_fill()
     
-    for i in range (points):
+    for i in range(points):
         t.forward(size)
         t.right(angle)
     t.end_fill()
 
 # Pääohjelma
-t.Screen () .bgcolor('black')
+t.Screen().bgcolor('black')
 
 while True:
 
     koordinaatti = t.pos();
-    x = m.floor(koordinaatti[0])
-    y = m.floor(koordinaatti [1])
+    x = m.floor(randint(-200, 200))
+    y = m.floor(randint(-200, 200))
     print(x,y)
-    ranPts = randint (2, 5) * 8 + 1
-    ranSize = randint (10, 50)
-    ranCol = (random(), random(), random())
-    draw_star (ranPts, ranSize, ranCol, x, y)
+    ranPts = randint(2, 5) * 2 + 1
+    ranSize = randint(10, 50)
+    ranCol =(random(), random(), random())
+    draw_star(ranPts, ranSize, ranCol, x, y)
     t.onscreenclick(t.goto(x,y))
