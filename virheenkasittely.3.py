27c42ddae4fcd55866428070992a1d3383555e52
AnniK <122267866+annikul@users.noreply.github.com>
2023-04-18 12:40:18 +0300

    virheenkäsittelyä

--- virheenkasittely.3.py
+++ virheenkasittely.3.py
@@ -1,12 +1,15 @@
-import traceback
+# Koodissa on virhe mutta se on hallinnassa
 
 def divide(x, y):
     try:
         result = x / y
-    
-    #except Exception:
-    #   traceback.print_exc()
-    except:
-        print('division by zero!')
-    finally:
-        print('heippa!')
+    except ZeroDivisionError:
+        print('division by zero')
+    else:
+        print('result = ',result)
+   
+# Nollallajako kaataa ohjelman
+divide(1, 0)
+
+# Mutta tämäkin lasketaan rivien (4, 6-10) takia
+divide(1, 1)
